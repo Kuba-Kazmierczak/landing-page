0d9352e7433d3e8d4adfc687f5a4e37741e57273
Kuba Kazmierczak <kbk.kazmierczak@gmail.com>
2023-06-19 12:43:21 +0200

    Navbar v1

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,27 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'
+import './Navbar.css';
+
 const Navbar = () => {
   const links = [
-    {label: "brand", path: "/"},
-    {label: "about", path: "/about"},
-    {label: "skills" , path: "/skills"},
-    {label: "portfolio", path: "/portfolio"},
-    {label: "contact", path: "/contact"}
-  ]
+    { label: 'brand', path: '/' },
+    { label: 'about', path: '/about' },
+    { label: 'skills', path: '/skills' },
+    { label: 'portfolio', path: '/portfolio' },
+    { label: 'contact', path: '/contact' }
+  ];
+
 
   const renderedLinks = links.map((link) => {
-    return <Link key={link.label} to={link.path}>
-      <li className="navbar-link">{link.label}</li>
-    </Link>
+    return (
+      <Link key={link.label} to={link.path}>
+        <li className="navbar-link" >
+          {link.label}
+        </li>
+      </Link>
+    );
   });
 
   return (
-    <div className='navbar'>
-      {renderedLinks}
+    <div className="navbar">
+      <div className="navbar-left">
+        <Link to="/">
+          <span className="nav-brand">{links[0].label}</span>
+        </Link>
+      </div>
+      <div className="navbar-right">
+        {renderedLinks.slice(1)};
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
-
