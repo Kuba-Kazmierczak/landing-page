fa1756daefd52b8fc907374261993abc288fa3e5
Kuba Kazmierczak <kbk.kazmierczak@gmail.com>
2023-11-13 21:32:24 +0000

    Added tailwing config, small tweaks to Navbar and hero sections

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,49 +4,25 @@ import Card from "../card/Card";
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div className="hero grid gap-4 grid-cols-2 grid-rows-2 text-center">
       <Card>
-        <img src="" />
-        <h1
-          style={{
-            textAlign: "center",
-            paddingTop: "20px",
-            paddingBottom: "20px",
-          }}
-        >
-          Hello there!
-        </h1>
-        <div
-          style={{
-            width: "50vw",
-            textAlign: "center",
-            opacity: "0.5",
-            margin: "0 auto",
-            backgroundColor: "#ffffff",
-            
-          }}
-        >
-          <p
-            style={{
-              textAlign: "center",
-              margin: "0 auto",
-              paddingTop: "20px",
-              paddingBottom: "20px",
-              width: "50vw",
-              justifyContent: "center",
-              fontSize: "20px",
-              fontWeight: "bold",
-              color: "black",
-            }}
-          >
-            Recently I decided that I want to change my professional career to
-            IT. I have been preparing for this for some time after completing
-            the Infoshare Academy course. I am currently learning and improving
-            my web developer skills as a self-taught Javascript-related techs
-            with the help of Udemy and more experienced colleauges . I deeply
-            believe that my perseverance will help me achieve this goal.
-          </p>
-        </div>
+        <h1 className="text-6xl m-8">Hello there!</h1>
+      </Card>
+      <Card>
+        <p className="font-bold m-8 align-middle">
+          Recently I decided that I want to change my professional career to IT.
+          I have been preparing for this for some time after completing the
+          Infoshare Academy course. I am currently learning and improving my web
+          developer skills as a self-taught Javascript-related techs with the
+          help of Udemy and more experienced colleauges . I deeply believe that
+          my perseverance will help me achieve this goal.
+        </p>
+      </Card>
+      <Card>
+        <h1>Img placeholder</h1>
+      </Card>
+      <Card>
+        <h1>More info...</h1>
       </Card>
     </div>
   );
