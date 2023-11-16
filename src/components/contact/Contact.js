6638ffa192b07211eaceb1aec04dfb071ec02f78
Kuba Kazmierczak <kbk.kazmierczak@gmail.com>
2023-11-16 18:28:35 +0000

    Input style changes

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,15 +2,17 @@ import React from "react";
 import "./Contact.css";
 
 const Contact = () => {
+  const inputStyle =
+    "bg-transparent border-slate-500 shadow-xl appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline";
   return (
     <div class="contact text-center items-center justify-center flex">
-      <form class="xs:w-full md:w-1/2 lg:w-1/3 shadow-md rounded px-8 pt-6 pb-8 mb-4 xl:w-2/6backdrop-blur-sm bg-blend-luminosity">
+      <form class="xs:w-full md:w-1/2 lg:w-1/3 shadow-md rounded px-8 pt-6 pb-8 mb-4 xl:w-2/6 backdrop-blur-sm">
         <div class="mb-4">
           <label class="block text-gray-700 text-lg font-bold mb-2" for="Name">
             Name
           </label>
           <input
-            className="bg-transparent shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className={inputStyle}
             id="username"
             type="text"
             placeholder=""
@@ -21,7 +23,7 @@ const Contact = () => {
             Email
           </label>
           <input
-            className=" bg-transparent shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className={inputStyle}
             id="Email"
             type="email"
             placeholder=""
@@ -31,19 +33,14 @@ const Contact = () => {
           <label class="block text-gray-700 text-lg font-bold mb-2" for="Email">
             Phone
           </label>
-          <input
-            className="bg-transparent shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="phone"
-            type="text"
-            placeholder=""
-          />
+          <input className={inputStyle} id="phone" type="text" placeholder="" />
         </div>
         <div class="mb-6">
           <label class="block text-gray-700 text-lg font-bold mb-2" for="Email">
             Message
           </label>
           <textarea
-            className="bg-transparent shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className={inputStyle}
             id="phone"
             type="text"
             placeholder=""
@@ -51,7 +48,7 @@ const Contact = () => {
         </div>
         <div class="flex items-center justify-center">
           <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:w-1/4 hover:ease-in hover:duration-300"
             type="button"
           >
             Send
