aa01e909afe1186acea06a9d324e4d64486a19e6
Kuba Kazmierczak <kbk.kazmierczak@gmail.com>
2023-11-16 17:59:44 +0000

    Contact section-form added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,11 +1,65 @@
-import React from 'react'
-import './Contact.css'
+import React from "react";
+import "./Contact.css";
+
 const Contact = () => {
   return (
-    <div className='contact'>
-      Contact
+    <div class="contact text-center items-center justify-center flex">
+      <form class="xs:w-full md:w-1/2 lg:w-1/3 shadow-md rounded px-8 pt-6 pb-8 mb-4 xl:w-2/6backdrop-blur-sm bg-blend-luminosity">
+        <div class="mb-4">
+          <label class="block text-gray-700 text-lg font-bold mb-2" for="Name">
+            Name
+          </label>
+          <input
+            className="bg-transparent shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder=""
+          />
+        </div>
+        <div class="mb-6">
+          <label class="block text-gray-700 text-lg font-bold mb-2" for="Email">
+            Email
+          </label>
+          <input
+            className=" bg-transparent shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="Email"
+            type="email"
+            placeholder=""
+          />
+        </div>
+        <div class="mb-6">
+          <label class="block text-gray-700 text-lg font-bold mb-2" for="Email">
+            Phone
+          </label>
+          <input
+            className="bg-transparent shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="phone"
+            type="text"
+            placeholder=""
+          />
+        </div>
+        <div class="mb-6">
+          <label class="block text-gray-700 text-lg font-bold mb-2" for="Email">
+            Message
+          </label>
+          <textarea
+            className="bg-transparent shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="phone"
+            type="text"
+            placeholder=""
+          />
+        </div>
+        <div class="flex items-center justify-center">
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Send
+          </button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -9,11 +9,11 @@ const Hero = () => {
         <h1 className="text-6xl m-8">Hello there!</h1>
       </Card>
       <Card>
-        <p className="font-bold m-8 align-middle">
+        <p className="font-bold m-8 align-middle shadow-2xl pt-10">
           Recently I decided that I want to change my professional career to IT.
           I have been preparing for this for some time after completing the
           Infoshare Academy course. I am currently learning and improving my web
-          developer skills as a self-taught Javascript-related techs with the
+          developer skills as a self-taught in Javascript-related techs with the
           help of Udemy and more experienced colleauges . I deeply believe that
           my perseverance will help me achieve this goal.
         </p>
